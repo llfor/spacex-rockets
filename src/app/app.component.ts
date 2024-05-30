25c6437683d8c8311bf5fe6ca3e4150e161c28e3
llforcada.develop <llforcada.develop@gmail.com>
2024-05-30 11:00:00 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spacex-app';
+  data: any[]=[];
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit() {
+    this.http.get('https://api.spacexdata.com/v4/rockets').subscribe((data: any) =>{
+       this.data= data;
+    })
+  }
 }
